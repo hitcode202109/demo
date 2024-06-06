80ee7ce600379a1412172299dff8ed2e024c9c4b
hitcode202109 <z2788002399@163.com>
2024-06-06 08:10:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000") // 允许来自localhost:3000的跨域请求
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
+
